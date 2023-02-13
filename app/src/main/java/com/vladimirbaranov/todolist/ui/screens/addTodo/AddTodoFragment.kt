983d49295f9ff7173903d6865437687a9ab74c59
Vladimir Baranov <vladimir.code@gmail.com>
2023-02-13 11:55:47 +0500

    Some updates

--- app/src/main/java/com/vladimirbaranov/todolist/ui/screens/addTodo/AddTodoFragment.kt
+++ app/src/main/java/com/vladimirbaranov/todolist/ui/screens/addTodo/AddTodoFragment.kt
@@ -1,9 +1,12 @@
 package com.vladimirbaranov.todolist.ui.screens.addTodo
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
@@ -58,11 +61,27 @@ class AddTodoFragment : DaggerFragment() {
         configEvents()
     }
 
+    override fun onResume() {
+        super.onResume()
+        binding?.etTodo?.apply {
+            requestFocus()
+            showKeyboard()
+        }
+    }
+
     private fun configTodoEditAndButton() {
         binding?.apply {
             etTodo.addTextChangedListener { editable ->
                 editable?.let { todoText ->
                     btAdd.isEnabled = todoText.isNotBlank()
+                }
+            }
+            etTodo.setOnEditorActionListener { _, actionId, event ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    etTodo.text?.let { viewModel.addTodo(it.toString()) }
+                    true
+                } else {
+                    false
                 }
             }
             btAdd.isEnabled = etTodo.text?.isNotBlank() ?: false
@@ -87,10 +106,17 @@ class AddTodoFragment : DaggerFragment() {
     }
 
     private fun onAddedEvent(todoAdded: TodoAdded) {
+        Toast.makeText(requireContext(), getString(R.string.todoAdded), Toast.LENGTH_SHORT).show()
         requireActivity().supportFragmentManager.popBackStack()
     }
 
     private fun onErrorEvent(error: Error) {
         Toast.makeText(requireContext(), getString(R.string.error), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun View.showKeyboard() {
+        val inputMethodManager =
+            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(this, 0)
     }
 }
